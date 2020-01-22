617fdb89a40960c4333db00bcd70d58bd7321472
Jake <rosedj1@gmail.com>
2020-01-22 11:24:47 -0500

    Made delta_pT/pT^2 distributions.

--- d0_Studies/setup.sh
+++ d0_Studies/setup.sh
@@ -6,10 +6,11 @@ export PYTHONPATH=$PYTHONPATH:${PWD}/leptonPtErrorCorrector/doCorrection/
 export PYTHONPATH=$PYTHONPATH:${PWD}/leptonPtErrorCorrector/doCorrection/tmp/
 export PYTHONPATH=$PYTHONPATH:${PWD}/doClosure/
 export PYTHONPATH=$PYTHONPATH:${PWD}/doClosure/ZClosure/
-export PYTHONPATH=$PYTHONPATH:/home/rosedj1/HiggsMeasurement/CMSSW_10_2_15/src/d0_Studies/
+export PYTHONPATH=$PYTHONPATH:/home/rosedj1/HiggsMeasurement/CMSSW_10_2_15/src/HiggsMassMeasurement/d0_Studies/
+
 export PATH=${PATH}:${PWD}/bin
 export BASE_PATH=${PWD}
 
-cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_8_0_32/src/
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc700/cms/cmssw/CMSSW_10_2_15/src/
 eval `scramv1 runtime -sh`
 cd -
