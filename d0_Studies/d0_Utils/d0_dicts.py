5cf5404cf036e6fd393253a2b32bde2ce9eafc37
Jake <rosedj1@gmail.com>
2020-02-17 21:51:29 -0500

    Made deltapT/(pT_gen)^2 vs d0*charge plots.
    Changing y-axis to remove the 10^(-4) factor

--- d0_Studies/d0_Utils/d0_dicts.py
+++ d0_Studies/d0_Utils/d0_dicts.py
@@ -2,3 +2,16 @@ charge_dict = {
     '+':'-13', 
     '-':'13',
 }
+
+color_dict = {
+    1:'black',
+    2:'red',
+    3:'green',
+    4:'blue',
+    5:'yellow',
+    6:'pink',
+    7:'turquoise',
+    8:'limegreen',
+    9:'darkviolet',
+    10:'orange',
+}
